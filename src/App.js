824eb04a193ea22916703f2a4e8b2e7989dcbdb7
Иосса Александр Николаевич <Aleksandr.Iossa@itech.ru>
2020-03-10 21:32:15 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Редактируйте <code>src/App.js</code> и сохраните для обновления
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Изучаем реакт!
         </a>
       </header>
     </div>
