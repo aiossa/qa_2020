653c04a8611217e2f17c43bcbfd39e632cc12ccc
aiossa <iossa92@gmail.com>
2020-07-29 08:55:32 +0300

    wrong lines

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,11 @@
 export let simpleCalculation = function simpleCalculation(a, b) {
     if (isNaN(a) || isNaN(b)) {
-        return 'Одно из значений или оба не числа'
-    }  else return a + b;
+     return 'Одно из значений или оба не числа'
+    }
+    return a + b;
 }
 
 export let compareTwoNumbers = function compareTwoNumbers(inp1, inp2) {
     if (Math.abs(inp1)> Math.abs(inp2)) return inp1
-    else return inp2
+     return inp2
 }
