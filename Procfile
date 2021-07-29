852a7f2c471ac994cb74bf3d8357495bb72b62b1
aiossa <iossa92@gmail.com>
2021-07-29 23:02:08 +0300

    new updates

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: npm run heroku_build
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {simpleCalculation} from './utils/utils';
+import {isNumberOddOrEven} from './utils/utils';
 
 
 function App() {
@@ -10,14 +10,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Это ветка Мастер! Мы деплоимся в Хероку
+          Это ветка testCoverage, которая поможет вам выполнить практическое задание.
         </p>
         <p>
           Редактируйте <code>src/App.js</code> и сохраните для обновления!
         </p>
         <p>
-            Result of calculation is {simpleCalculation(10,4)}
-          </p>
+          {isNumberOddOrEven(10,4)}
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
